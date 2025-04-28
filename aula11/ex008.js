1b2fa11ce545c1190f15aad3825457d66f7db1dc
Vinícius Brandão <viniciusbran153@gmail.com>
2025-04-28 15:25:52 -0300

    if e else

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+//condições em js
+var vm = 60
+if (vm > 60 ){
+    console.log(`Sua velocidade é ${vm}Km/h. Você ultrapassou o limite de velocidade. MULTADO!!`)
+}
+else{
+    console.log(`Sua velocidade é ${vm}Km/h. Dirija com segurança! Tenha um ótimo dia!`)
+}
+
