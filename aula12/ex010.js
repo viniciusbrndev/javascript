0df855be1b60e921c95d97fe1a1c1fb029a8bb4a
Vinícius Brandão <viniciusbran153@gmail.com>
2025-04-29 14:14:54 -0300

    desafio01js

---
+++ aula12/ex010.js
@@ -0,0 +1,11 @@
+var media = 9
+if (media < 6){
+    console.log ('Ok!')
+}
+else if (media < 9){
+    console.log ('Muito bom!')
+}
+else{
+    console.log ('Incrível!!!')
+}
+
---
+++ aula12/ex011.js
@@ -0,0 +1,10 @@
+var idade = 65
+if (idade < 16){
+    console.log ('Nào vota')
+}
+else if (idade < 18 || idade >= 65) {
+    console.log ('Voto não obrigatório')
+}
+else{
+    console.log ('Voto obrigatório!')
+}
